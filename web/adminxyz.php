a9968874f0ac54869f33c284972f8d4288aea5f2
adityaeka26 <aditya.eka409@gmail.com>
2022-03-31 14:56:22 +0700

    add captcha in login admin

--- web/adminxyz.php
+++ web/adminxyz.php
@@ -2,6 +2,8 @@
 include "header.php";
 ?>
 
+<script src='https://www.google.com/recaptcha/api.js' async defer></script>
+
 <div class="login-clean">
 <form method="post" action="<?php echo $host;?>function/actSigninAdmin.php">
              <!-- if signup failed -->
@@ -10,6 +12,10 @@ include "header.php";
             ?>
                 <b style="display: block;position: relative;text-align:center; color: rgb(244,71,107)">Email dan Password tidak sesuai</b>
             <?php } else if(@$_GET['status'] == 'success'){ ?>
+                <?php } else if(@$_GET['status'] == 'failed-captcha'){ ?>
+            <b style="display: block;position: relative;text-align:center; color: rgb(244,71,107)">Captcha tidak sesuai</b>
+                <?php } else if(@$_GET['status'] == 'csrf'){ ?>
+            <b style="display: block;position: relative;text-align:center; color: rgb(244,71,107)">Error CSRF</b>
                 <b style="display: block;position: relative;text-align:center; color: rgb(244,71,107)">Signup Success</b>
             <?php } ?>
             <!--  -->
@@ -18,6 +24,7 @@ include "header.php";
             <div class="illustration"><i class="fa fa-ticket"></i></div>
             <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email"></div>
             <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
+            <div class="g-recaptcha" data-sitekey="<?php echo getenv('RECAPTCHA_SITE_KEY') ?>"></div>
             <div class="form-group"><button class="btn btn-primary btn-block" type="submit">Sign In</button></div><a class="forgot" href="forgotPassword.php">Forgot your email or password?</a></form>
 </div>
 
