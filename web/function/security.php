8b75582c499f86d01490660a79c5bbe2fa462cb8
Hamim Sazadah <hamim.sazadah@gmail.com>
2022-03-31 11:27:14 +0700

    update sanitize

--- web/function/security.php
+++ web/function/security.php
@@ -1,15 +1,6 @@
 <?php
-include '../conn.php';
 function sanitize($str){
-    $str =  $conn->real_escape_string(htmlspecialchars($str,ENT_QUOTES));
+    include '../conn.php';
+    return  mysqli_real_escape_string($conn,htmlspecialchars($str,ENT_QUOTES));
 }
-
-
-// function strngPassword($str){
-//     if(preg_match('/[0-9]/', $str) && preg_match('/[A-Z]/', $str) && preg_match('/[a-z]/', $str) && preg_match('/[@#$%^&*-+~]/', $str) && strlen($str)>=12 && strlen($str)<=128){
-
-//     }else{
-
-//     }
-// }
 ?>
