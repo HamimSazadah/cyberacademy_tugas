e90348fb4ecd285c3d64a97c7a98d03195d8da84
adityaeka26 <aditya.eka409@gmail.com>
2022-03-31 11:54:31 +0700

    captcha

--- web/function/actSignin.php
+++ web/function/actSignin.php
@@ -5,6 +5,22 @@
     $email = @$_POST['email'];
     $password = sha1(@$_POST['password']);
 
+    if (isset($_POST['g-recaptcha-response'])) {
+        $captcha=$_POST['g-recaptcha-response'];
+    }
+    if (!$captcha) {
+        header('Location: '.$host.'signin.php?status=failed-captcha' );
+        exit;
+    }
+    $secretKey = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe";
+    $url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($secretKey) .  '&response=' . urlencode($captcha);
+    $response = file_get_contents($url);
+    $responseKeys = json_decode($response,true);
+    if(!$responseKeys["success"]) {
+        header('Location: '.$host.'signin.php?status=failed-captcha' );
+        exit;
+    }
+
     $sql = "SELECT * FROM users where email = '$email' and password = '$password'";
     $result = $conn->query($sql);
 
--- web/signin.php
+++ web/signin.php
@@ -2,6 +2,8 @@
 include "header.php";
 ?>
 
+<script src='https://www.google.com/recaptcha/api.js' async defer></script>
+
 <div class="login-clean">
         <form method="post" action="<?php echo $host;?>function/actSignin.php">
              <!-- if signup failed -->
@@ -9,6 +11,8 @@ include "header.php";
                 if(@$_GET['status'] == 'failed'){
             ?>
                 <b style="display: block;position: relative;text-align:center; color: rgb(244,71,107)">Email dan Password tidak sesuai</b>
+            <?php } else if(@$_GET['status'] == 'failed-captcha'){ ?>
+                <b style="display: block;position: relative;text-align:center; color: rgb(244,71,107)">Captcha tidak sesuai</b>
             <?php } else if(@$_GET['status'] == 'success'){ ?>
                 <b style="display: block;position: relative;text-align:center; color: rgb(244,71,107)">Signup Success</b>
             <?php } ?>
@@ -18,6 +22,7 @@ include "header.php";
             <div class="illustration"><i class="fa fa-ticket"></i></div>
             <div class="form-group"><input class="form-control" type="text" name="email" placeholder="Email"></div>
             <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
+            <div class="g-recaptcha" data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"></div>
             <div class="form-group"><button class="btn btn-primary btn-block" type="submit">Sign In</button></div><a class="forgot" href="forgotPassword.php">Forgot your email or password?</a></form>
 </div>
 
