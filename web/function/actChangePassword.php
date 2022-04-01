041b9616ec14f1bc27da32e20d0d22a9aba14573
adityaeka26 <aditya.eka409@gmail.com>
2022-04-01 10:26:47 +0700

    fix change password (add form current password & confirm password)

--- web/function/actChangePassword.php
+++ web/function/actChangePassword.php
@@ -1,5 +1,6 @@
 <?php
 include "../conn.php";
+include "security.php";
 
 @session_start();
 
@@ -9,7 +10,30 @@ if(!$id){
     header('location:'.$host.'signin.php');
 }
 
-$password = sha1($_POST['password']);
+$sql = "SELECT * FROM users where id = '$id'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $email = $row['email'];
+        $currentPasswordDb = $row['password'];
+    }
+} else {
+    header('Location: '.$host.'changePassword.php?status=failed-id');
+    exit;
+}
+
+$currentPassword = sha1(sanitize($_POST['current_password']).$email.getenv('SALT'));
+if ($currentPassword != $currentPasswordDb) {
+    header('Location: '.$host.'changePassword.php?status=failed-current');
+    exit;
+}
+
+$password = sha1(sanitize($_POST['password']).$email.getenv('SALT'));
+$confirmPassword = sha1(sanitize($_POST['confirm_password']).$email.getenv('SALT'));
+if ($password != $confirmPassword) {
+    header('Location: '.$host.'changePassword.php?status=failed-confirm');
+    exit;
+}
 
 // update data
 $user = "UPDATE users SET password = '$password' WHERE id = $id";
