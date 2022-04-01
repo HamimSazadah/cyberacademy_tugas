048fec9021bfb483751a8fe0a865769bafb7f841
Hamim Sazadah <hamim.sazadah@gmail.com>
2022-04-01 13:11:27 +0700

    add mime check

--- web/function/actUpdateProfile.php
+++ web/function/actUpdateProfile.php
@@ -12,49 +12,53 @@ $flood = new FloodDetection();
 $flood->check();
 
 $fullname = sanitize(@$_POST['fullname']);
-$email = filter_var(@$_POST['email'],FILTER_SANITIZE_EMAIL);
+$email = filter_var(@$_POST['email'], FILTER_SANITIZE_EMAIL);
 
 $id = @$_SESSION['id'];
 $phone = 0;
 
-if(@$_POST['phone'] != '' || @$_POST['phone'] != null){
-    $phone = filter_var(@$_POST['phone'],FILTER_SANITIZE_NUMBER_INT);
+if (@$_POST['phone'] != '' || @$_POST['phone'] != null) {
+    $phone = filter_var(@$_POST['phone'], FILTER_SANITIZE_NUMBER_INT);
 }
 
-$allowed_types = ['image/png','image/jpg','image/jpeg'];
+$allowed_types = ['image/png', 'image/jpg', 'image/jpeg'];
 // mitra
 $fileName = $_FILES['userfile']['name'];
 $fileType = $_FILES['userfile']['type'];
 
-if (!empty($_POST) && $_SESSION['csrf'] != $_POST['csrf']) { 
+if (!empty($_POST) && $_SESSION['csrf'] != $_POST['csrf']) {
     $_SESSION['csrf'] =  bin2hex(random_bytes(35));
-    echo 'gagal';
+    echo 'gagal 1';
     exit;
 }
 
 
- // nama direktori upload
+// nama direktori upload
 $namaDir = '../files/';
 
 // membuat path nama direktori + nama file.
 // $pathFile = $namaDir.$fileName;
-$newfilename=getOldIdentity($conn);
+$newfilename = getOldIdentity($conn);
 if ($fileName) {
-    if (!in_array($fileType,$allowed_types)){
+    if (!in_array($fileType, $allowed_types)) {
         $_SESSION['csrf'] =  bin2hex(random_bytes(35));
         echo "File gagal diupload.";
         exit;
     }
-    $oldIdentity = $namaDir.$newfilename;
+    $oldIdentity = $namaDir . $newfilename;
     unlink($oldIdentity);
     // memindahkan file ke temporary
     $tmpName  = $_FILES['userfile']['tmp_name'];
 
     $temp = explode(".", $fileName);
     $newfilename = round(microtime(true)) . '.' . end($temp);
-    $pathFile = $namaDir.$newfilename;
+    $pathFile = $namaDir . $newfilename;
 
-    // proses upload file dari temporary ke path file
+    if (!in_array(mime_content_type($_FILES['userfile']['tmp_name']), $allowed_types)) {
+        echo 'gagal 2 ' . mime_content_type($_FILES['userfile']['tmp_name']);
+        exit;
+    }
+    //proses upload file dari temporary ke path file
     if (!move_uploaded_file($_FILES['userfile']['tmp_name'], $pathFile)) {
         $_SESSION['csrf'] =  bin2hex(random_bytes(35));
         var_dump($_FILES['userfile']['error']);
@@ -63,28 +67,28 @@ if ($fileName) {
     }
 }
 
-updateUser($conn,$email);
-updateProfile($conn,$fullname,$phone,$newfilename);
+updateUser($conn, $email);
+updateProfile($conn, $fullname, $phone, $newfilename);
 $conn->close();
 $_SESSION['csrf'] =  bin2hex(random_bytes(35));
-header('Location: '.$host.'profile.php?status=success');
+header('Location: ' . $host . 'profile.php?status=success');
 
 
-function updateUser($conn,string $email)
+function updateUser($conn, string $email)
 {
     $user = "UPDATE users SET email = ? WHERE id = ?";
     $stmt = $conn->prepare($user);
     $userid = @$_SESSION['id'];
-    $stmt->bind_param('si',$email,$userid);
+    $stmt->bind_param('si', $email, $userid);
     execute($stmt);
 }
 
-function updateProfile($conn,string $fullname,string $phone,string $identity)
+function updateProfile($conn, string $fullname, string $phone, string $identity)
 {
     $user = "UPDATE user_profile SET fullname = ?, phone = ?, identity_card =? WHERE id_user = ?";
     $stmt = $conn->prepare($user);
     $userid = @$_SESSION['id'];
-    $stmt->bind_param('sisi',$fullname,$phone,$identity,$userid);
+    $stmt->bind_param('sisi', $fullname, $phone, $identity, $userid);
     execute($stmt);
 }
 
@@ -92,12 +96,12 @@ function getOldIdentity($conn)
 {
     $id = @$_SESSION['id'];
     $stmt = $conn->prepare("SELECT identity_card FROM user_profile WHERE id_user=?");
-    $stmt->bind_param('i',$id);
+    $stmt->bind_param('i', $id);
     $stmt->execute();
     $result = $stmt->get_result();
     $stmt->close();
     $row = $result->fetch_assoc();
-    if ($row['identity_card'] != null){
+    if ($row['identity_card'] != null) {
         return $row['identity_card'];
     }
     return '';
